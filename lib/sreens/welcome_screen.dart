1fd93669eb8544d9958d2a9e7376add209b9227d
Garaad Amiin <garaadamiin3@gmail.com>
2022-12-08 19:51:18 +0300

    add new screen page welcome

---
+++ lib/sreens/welcome_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      height: MediaQuery.of(context).size.height,
+      width: double.infinity,
+      decoration: const BoxDecoration(color: Color(0xFF093631)),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          const SizedBox(
+              height: 130,
+              width: 220,
+              child: Image(image: AssetImage("assets/logo.png"))),
+        ],
+      ),
+    ));
+  }
+}
