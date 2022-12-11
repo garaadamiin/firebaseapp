1c1b069bbb1fa0e1f3016d1b16043ddbb337660f
Garaad Amiin <garaadamiin3@gmail.com>
2022-12-11 21:13:43 +0300

    add arrow back in Login page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -5,16 +5,33 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        height: MediaQuery.of(context).size.height,
-        width: double.infinity,
-        child: Column(children: [
-          Container(
-            height: 50,
-            width: 50,
-          )
-        ]),
+    return SafeArea(
+      child: Scaffold(
+        body: Container(
+          height: MediaQuery.of(context).size.height,
+          width: double.infinity,
+          child: Column(children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Container(
+                  height: 50,
+                  width: 50,
+                  decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 1),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: IconButton(
+                    icon: Icon(Icons.arrow_back_ios_sharp),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                ),
+              ),
+            )
+          ]),
+        ),
       ),
     );
   }
