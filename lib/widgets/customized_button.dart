1b966f61e8262c7bce99a0e730dc785981af0745
Garaad Amiin <garaadamiin3@gmail.com>
2022-12-11 18:45:08 +0300

    add customized button

--- lib/widgets/customized_button.dart
+++ lib/widgets/customized_button.dart
@@ -1,24 +1,36 @@
 import 'package:flutter/material.dart';
 
 class customizedButton extends StatelessWidget {
-  const customizedButton({super.key});
+  final String? buttonText;
+  final Color? buttonColor;
+  final Color? textColor;
+  final VoidCallback? onPressed;
+  const customizedButton(
+      {super.key,
+      this.buttonText,
+      this.buttonColor,
+      this.textColor,
+      this.onPressed});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Container(
-        height: 80,
-        width: MediaQuery.of(context).size.width,
-        decoration: BoxDecoration(
-            color: Colors.black,
-            border: Border.all(width: 1, color: Colors.white),
-            borderRadius: BorderRadius.circular(10)),
-        child: const Center(
-            child: Text(
-          "LogIn",
-          style: TextStyle(color: Colors.white, fontSize: 20),
-        )),
+      child: InkWell(
+        onTap: onPressed,
+        child: Container(
+          height: 80,
+          width: MediaQuery.of(context).size.width,
+          decoration: BoxDecoration(
+              color: buttonColor,
+              border: Border.all(width: 1, color: Colors.white),
+              borderRadius: BorderRadius.circular(10)),
+          child: Center(
+              child: Text(
+            buttonText!,
+            style: TextStyle(color: textColor, fontSize: 20),
+          )),
+        ),
       ),
     );
   }
