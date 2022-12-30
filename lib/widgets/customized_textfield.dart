641704e23c6f99c60d49c3fb0750a7b7e04cfa4a
Garaad Amiin <garaadamiin3@gmail.com>
2022-12-30 19:22:51 +0300

    modified Text Field customize

--- lib/widgets/customized_textfield.dart
+++ lib/widgets/customized_textfield.dart
@@ -15,10 +15,30 @@ class CustomizedTextfield extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: TextField(
+        keyboardType: isPassword!
+            ? TextInputType.visiblePassword
+            : TextInputType.emailAddress,
+        enableSuggestions: isPassword! ? false : true,
+        autocorrect: isPassword! ? false : true,
         obscureText: isPassword ?? true,
         controller: myController,
         decoration: InputDecoration(
-          fillColor: Color(0xffE8ECF4),
+          suffixIcon: isPassword!
+              ? IconButton(
+                  icon: const Icon(
+                    Icons.remove_red_eye,
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {},
+                )
+              : null,
+          enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Color(0xffE8ECF4), width: 1),
+              borderRadius: BorderRadius.circular(10)),
+          focusedBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Color(0xffE8ECF4), width: 1),
+              borderRadius: BorderRadius.circular(10)),
+          fillColor: const Color(0xffE8ECF4),
           filled: true,
           hintText: hintText,
           border: OutlineInputBorder(borderRadius: BorderRadius.circular(10)),
