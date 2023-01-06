c4539a5947ad8cb7a56abbe54a6daf5c3392e8fb
Garaad Amiin <garaadamiin3@gmail.com>
2023-01-07 01:04:17 +0300

    otp

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,7 @@
+import 'package:flutter/material.dart';
+
 import 'package:firebase_app_flutter/screens/forgotPassword_screen.dart';
 import 'package:firebase_app_flutter/screens/signup_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 import '../widgets/auth_button.dart';
 import '../widgets/customized_button.dart';
