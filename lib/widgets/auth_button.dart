ad3bd6c06256b6660e81fb103f12a545826016ab
Garaad Amiin <garaadamiin3@gmail.com>
2023-01-14 22:05:09 +0300

    modified small error

--- lib/widgets/auth_button.dart
+++ lib/widgets/auth_button.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class authButton extends StatelessWidget {
+class AuthButton extends StatelessWidget {
+  const AuthButton({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Padding(
